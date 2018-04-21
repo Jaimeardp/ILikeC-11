9240e00e8f812df6f939bc82871f8478caaf454b
jaimeardp <jaimeardp@gmail.com>
2018-04-21 06:18:44 -0500

    add *ptrs

--- src/Pointers/pointers01.cpp
+++ src/Pointers/pointers01.cpp
@@ -9,11 +9,18 @@ int main(){
     char * it;
     char * av;
 
-    it = arr;
+    it = arr; // it apunta a arr[0]
 
     av = it + 3 ;// size(char) -> numero de bytes
 
     cout<<"Donde apunta mi caracter -> "<<*av <<endl; // &av -> numero de direccion de memoria
+
+    // Iterando con un puntero 
+    cout<<"Iterando una lista con punteros"<<endl;
+    it = arr; //Coloco el puntero al inicio
+    for(int i=0 ; i<6; i++){
+        cout<< *(it + i) <<endl;
+    }
 
     return 0;
 
